ccfa78b89c0322bb2eba587f0efa7b886ef8a4fd
koalaa13 <nooshe981@gmail.com>
2020-10-10 16:10:43 +0300

    Change opening image in fullscreen to another activity

---
+++ app/src/main/java/com/example/imagesviewer/FullScreenImageActivity.kt
@@ -0,0 +1,44 @@
+package com.example.imagesviewer
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.imagesviewer.MainActivity.Companion.imagesMap
+import kotlinx.android.synthetic.main.fullhd_image.*
+import java.util.*
+
+class FullScreenImageActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "FullScreenImageActivity"
+    }
+
+    private val fullhdImagesDownloads: Queue<DownloadImage> = LinkedList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fullhd_image)
+        Log.i(TAG, "onCreate")
+        val closeFullScreen = Intent(this, MainActivity::class.java)
+
+        val onCLick = View.OnClickListener() {
+            while (!fullhdImagesDownloads.isEmpty()) {
+                val cur = fullhdImagesDownloads.poll()
+                cur?.cancel(true)
+            }
+            startActivity(closeFullScreen)
+        }
+        fullhd_image.setOnClickListener(onCLick)
+        fullhd_image_downloading_progressbar.setOnClickListener(onCLick)
+
+        fullhdImagesDownloads.add(
+            DownloadImage(
+                imagesMap,
+                fullhd_image,
+                listOf(fullhd_image_downloading_progressbar)
+            )
+        )
+        fullhdImagesDownloads.peek()?.execute(intent.getStringExtra("linkToDownload"))
+    }
+}
--- app/src/main/java/com/example/imagesviewer/MainActivity.kt
+++ app/src/main/java/com/example/imagesviewer/MainActivity.kt
@@ -1,53 +1,36 @@
 package com.example.imagesviewer
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fullhd_image.*
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val TAG = "MainActivity"
+        var imagesMap = ConcurrentHashMap<String, Bitmap>()
+        private var imagesList = emptyList<Image>().toMutableList()
     }
-
-    var imagesList = emptyList<Image>().toMutableList()
-    private val imagesMap = ConcurrentHashMap<String, Bitmap>()
-    private val fullhdImagesDownloads: Queue<DownloadImage> = LinkedList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i(TAG, "onCreate")
         val viewManager = LinearLayoutManager(this)
-
-        val onCLick = View.OnClickListener() {
-            while (!fullhdImagesDownloads.isEmpty()) {
-                val cur = fullhdImagesDownloads.poll()
-                cur?.cancel(true)
-            }
-            it.visibility = View.GONE
-        }
-        fullhd_image.setOnClickListener(onCLick)
-        fullhd_image_downloading_progressbar.setOnClickListener(onCLick)
-
+        val openFullScreen = Intent(this, FullScreenImageActivity::class.java)
 
         myRecyclerView.apply {
             layoutManager = viewManager
             adapter = ImageAdapter(imagesList, imagesMap) {
-                fullhdImagesDownloads.add(
-                    DownloadImage(
-                        imagesMap,
-                        fullhd_image,
-                        listOf(fullhd_image_downloading_progressbar)
-                    )
-                )
-                fullhdImagesDownloads.peek()?.execute(it.fullLink)
-                fullhd_image_downloading_progressbar.visibility = View.VISIBLE
+                startActivity(openFullScreen.apply {
+                    putExtra("linkToDownload", it.fullLink)
+                })
             }
         }
 
